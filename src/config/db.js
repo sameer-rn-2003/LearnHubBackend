13377a4ac297fb416a211a0d7d738ff95256df8e
Sameer <ssingh.psllp@gmail.com>
2026-03-05 10:11:17 +0530

    added conng

--- src/config/db.js
+++ src/config/db.js
@@ -4,8 +4,11 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const connectDB = async () => {
-  const mongoUri = process.env.MONGO_URI;
-  const mongoDbName = process.env.MONGO_DB_NAME;
+  // const mongoUri = process.env.MONGO_URI;
+  // const mongoDbName = process.env.MONGO_DB_NAME;
+
+  const mongoUri = "mongodb+srv://sameer08github_db_user:7CZzj2CuY37lWdLt@cluster0.ovdxkpm.mongodb.net/";
+  const mongoDbName = "node_template";
 
   await mongoose.connect(mongoUri, {
     dbName: mongoDbName
